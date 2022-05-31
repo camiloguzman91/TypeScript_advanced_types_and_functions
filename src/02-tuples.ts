2263804208db0da9a49acc22ea6bb1f8fe73f055
Camilo Guzman <camiloge_91@hotmail.com>
2022-05-31 12:07:05 -0500

    tuples

---
+++ src/02-tuples.ts
@@ -0,0 +1,8 @@
+const prices: (number | string)[] = [1,3,5,1,2, 'asd'];
+prices.push(1);
+prices.push('1');
+
+const user: [string, number] = ['camilo', 15];
+
+let user1: [string, number, boolean];
+user1 = ['camilo', 31, true];
