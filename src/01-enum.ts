3feb9e2d022649c1ca750ce3f5dcb581a82d525d
Camilo Guzman <camiloge_91@hotmail.com>
2022-05-31 11:48:41 -0500

    enums

---
+++ src/01-enum.ts
@@ -0,0 +1,15 @@
+enum ROLES {
+  ADMIN = "admin",
+  SELLER = "seller",
+  COSTUMER = "costumer",
+}
+
+type User = {
+  username: string;
+  role: ROLES;
+}
+
+const camiUser: User = {
+  username: 'camilo',
+  role: ROLES.ADMIN
+}
