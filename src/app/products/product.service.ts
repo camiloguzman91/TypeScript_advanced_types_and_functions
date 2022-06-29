b00a51fa4602d4d0c3a1709d5d4bae1969fd90de
Camilo Guzman <camiloge_91@hotmail.com>
2022-06-29 12:51:49 -0500

    readonly

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  //data.id = 'hello';
+  //data.createdAt = new Date(1994, 5, 5);
   products.push(data);
 }
