82fc67c099a7324a7e40636a59f165243d04be4f
Camilo Guzman <camiloge_91@hotmail.com>
2022-06-30 22:16:15 -0500

    crud

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,11 +1,16 @@
-import { Category } from './../categories/category.model';
 import { BaseModel } from './../base.mode';
+import { Category } from './../categories/category.model';
 
-export type Sizes = "S" | "M" | "L" | "XL";
-
+export type Sizes = 'S' | 'M' | 'L' | 'XL';
 export interface Product extends BaseModel {
   title: string;
+  image: string;
+  description: string;
   stock: number;
   size?: Sizes;
+  color: string;
+  price: number;
   category: Category;
+  isNew: boolean;
+  tags: string[];
 }
