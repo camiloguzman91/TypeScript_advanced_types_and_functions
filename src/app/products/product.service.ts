d9ba6dded6b0fe411ca727b7ccf7f11a297849bf
Camilo Guzman <camiloge_91@hotmail.com>
2022-07-02 21:56:21 -0500

    omit_pick_type

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,11 +1,27 @@
+import { faker } from '@faker-js/faker';
+
 import { Product } from "./product.model";
+import { CreateProductDto } from "./product.dto";
 
 export const products: Product[] = [];
 
-export const addProduct = (data: Product) => {
+export const addProduct = (data: CreateProductDto): Product => {
   //data.id = 'hello';
   //data.createdAt = new Date(1994, 5, 5);
-  products.push(data);
+  const newProduct = {
+    ...data,
+    id: faker.datatype.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: data.categoryId,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct);
+  return newProduct;
 }
 
 export const updateProduct = (id: string, changes: Product) => {
